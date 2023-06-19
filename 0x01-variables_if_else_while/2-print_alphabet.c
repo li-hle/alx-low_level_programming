a38a066c107d40e2532ab8a8285cd1f0543a243d
li-hle <malihlemntungwa@gmail.com>
2023-06-19 08:13:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lh;
+
+	for (lh = 'a'; lh <= 'z'; lh++)
+	{
+		putchar(lh);
+	}
+	putchar('\n')
+	return (0);
+}
