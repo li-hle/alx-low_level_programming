7d37f8fa01684c6eb7dad20a285c177c0bd2ceb6
li-hle <malihlemntungwa@gmail.com>
2023-09-21 08:21:51 -0700

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/li-hle/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib101-makemewin.so
-export LD_PRELOAD=/tmp/nrandom.so
